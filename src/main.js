2b7c391845bf543bc4d713d51befd570da962329
Zakky <zakkysurya@gmail.com>
2024-10-29 16:46:49 +0700

    updated pinia to use in vuejs

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from "@/App.vue";
 import router from "@plugins/router";
 
 const app = createApp(App);
-app.use(createPinia());
+const pinia = createPinia();
+app.use(pinia);
 app.use(router);
 app.mount("#app");
